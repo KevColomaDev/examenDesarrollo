fd13a669aa1729e6a6bcf22fbddf5b3dd88b8b3c
Kevin <kev.enrri@hotmail.com>
2024-06-20 08:07:47 -0500

    feat: cors & .en.example

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import { router } from './routers/users.js'
 import { router as admins } from './routers/admins.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 // Routes
 app.use('/examen/v1', router)
